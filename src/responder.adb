2683e7987289e8e28567b241fd0f8d55db67d2d3
Johannes Kliemann <kliemann@componolit.com>
2021-08-31 18:57:17 +0200

    Prototype test case responder and proxy
    ref #1
    
    [no ci]

--- src/responder.adb
+++ src/responder.adb
@@ -10,13 +10,21 @@ procedure Responder
 is
    Listener : Server.Server_Type := Server.Create_Server;
    Connection : Server.Server_Type;
+   procedure Signal_Error (Signal_Error : out Boolean;
+                           Value        :     Boolean)
+   is
+   begin
+      Signal_Error := Value;
+      Ada.Command_Line.Set_Exit_Status (1);
+   end Signal_Error;
 begin
    Server.Listen (Listener, 2324, Connection);
    declare
       package TCP_Channel is new Channel (Connection);
       package Responder is new RFLX.SPDM.Responder (TCP_Channel.Has_Data,
                                                     TCP_Channel.Receive,
-                                                    TCP_Channel.Send);
+                                                    TCP_Channel.Send,
+                                                    Signal_Error);
    begin
       Responder.Run;
    end;
