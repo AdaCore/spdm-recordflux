f4e09a3d64424899964ed5baa1baaaad82e46c76
Johannes Kliemann <kliemann@componolit.com>
2021-09-07 16:15:15 +0200

    Respond with error to unsupported request
    ref #1

--- tools/run_responder.sh
+++ tools/run_responder.sh
@@ -1,11 +1,7 @@
 #!/bin/bash
 
-set -em
+set -e
 
 build/responder/responder &
 sleep 1 && build/responder/proxy &
-sleep 2 && build/spdm_emu/bin/spdm_requester_emu --trans NONE &
-
-fg %$(jobs | grep build/responder/responder | cut -d " " -f1 | tr -d "[]")
-
-jobs -p | xargs -r kill
+sleep 2 && build/spdm_emu/bin/spdm_requester_emu --trans NONE
