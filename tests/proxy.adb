0c3b3524d4848b7db5ffcec95d2da752de0165d1
Alexander Senier <senier@componolit.com>
2021-12-16 13:58:43 +0100

    Adapt dynamic validation tests
    Ref. #31

--- tests/proxy.adb
+++ tests/proxy.adb
@@ -6,27 +6,89 @@ with Ada.Command_Line;
 with Ada.Text_IO;
 with GNAT.Traceback.Symbolic;
 with RFLX.SPDM_Proxy.Proxy;
+with RFLX.RFLX_Types;
+with RFLX.RFLX_Builtin_Types;
 
 procedure Proxy
 is
-   Listener : Server.Server_Type := Server.Create_Server;
+   use type RFLX.RFLX_Types.Index;
+   use type RFLX.RFLX_Types.Length;
+
+   Listener          : Server.Server_Type := Server.Create_Server;
    Server_Connection : Server.Server_Type;
    Client_Connection : Client.Connection_Type;
+   Buffer            : RFLX.RFLX_Types.Bytes (RFLX.RFLX_Types.Index'First .. RFLX.RFLX_Types.Index'First + 4095);
+   Length            : RFLX.RFLX_Builtin_Types.Length;
+
+   package Responder_Channel is new Channel (Client_Connection);
+   package Emu_Channel is new Channel (Server_Connection);
+   package SPDM_Proxy is new RFLX.SPDM_Proxy.Proxy;
 begin
+   SPDM_Proxy.Initialize;
    Client.Connect (2324, Client_Connection);
    Server.Listen (Listener, 2323, Server_Connection);
-   declare
-      package SPDM_Channel is new Channel (Client_Connection);
-      package Emu_Channel is new Channel (Server_Connection);
-      package SPDM_Proxy is new RFLX.SPDM_Proxy.Proxy (Emu_Channel.Has_Data,
-                                                       Emu_Channel.Receive,
-                                                       Emu_Channel.Send,
-                                                       SPDM_Channel.Has_Data,
-                                                       SPDM_Channel.Receive,
-                                                       SPDM_Channel.Send);
-   begin
+
+   while SPDM_Proxy.Active loop
+
+      pragma Loop_Invariant (SPDM_Proxy.Initialized);
+
+      if SPDM_Proxy.Has_Data (SPDM_Proxy.C_Emu_Transport) then
+         declare
+            BS : constant RFLX.RFLX_Types.Length := SPDM_Proxy.Read_Buffer_Size (SPDM_Proxy.C_Emu_Transport);
+         begin
+            if Buffer'Length >= BS then
+               SPDM_Proxy.Read (SPDM_Proxy.C_Emu_Transport,
+                                Buffer (Buffer'First .. Buffer'First - 2 + RFLX.RFLX_Types.Index (BS + 1)));
+               Emu_Channel.Send (Buffer (Buffer'First .. Buffer'First - 2 + RFLX.RFLX_Types.Index (BS + 1)));
+            end if;
+         end;
+      end if;
+
+      if SPDM_Proxy.Needs_Data (SPDM_Proxy.C_Emu_Transport) then
+         declare
+            BS : constant RFLX.RFLX_Types.Length := SPDM_Proxy.Write_Buffer_Size (SPDM_Proxy.C_Emu_Transport);
+         begin
+            Emu_Channel.Receive (Buffer, Length);
+            if Length > 0 and Length <= BS then
+               SPDM_Proxy.Write
+                  (SPDM_Proxy.C_Emu_Transport,
+                   Buffer (Buffer'First .. Buffer'First +  RFLX.RFLX_Builtin_Types.Index (Length) - 1));
+            end if;
+         end;
+      end if;
+
+      if SPDM_Proxy.Has_Data (SPDM_Proxy.C_SPDM_Transport) then
+         declare
+            BS : constant RFLX.RFLX_Types.Length := SPDM_Proxy.Read_Buffer_Size (SPDM_Proxy.C_SPDM_Transport);
+         begin
+            if Buffer'Length >= BS then
+               SPDM_Proxy.Read (SPDM_Proxy.C_SPDM_Transport,
+                                Buffer (Buffer'First .. Buffer'First - 2 + RFLX.RFLX_Types.Index (BS + 1)));
+               Responder_Channel.Send (Buffer (Buffer'First .. Buffer'First - 2 + RFLX.RFLX_Types.Index (BS + 1)));
+            end if;
+         end;
+      end if;
+
+      if SPDM_Proxy.Needs_Data (SPDM_Proxy.C_SPDM_Transport) then
+         declare
+            BS : constant RFLX.RFLX_Types.Length := SPDM_Proxy.Write_Buffer_Size (SPDM_Proxy.C_SPDM_Transport);
+         begin
+            Responder_Channel.Receive (Buffer, Length);
+            if Length > 0 and Length <= BS then
+               SPDM_Proxy.Write
+                  (SPDM_Proxy.C_SPDM_Transport,
+                   Buffer (Buffer'First .. Buffer'First +  RFLX.RFLX_Builtin_Types.Index (Length) - 1));
+            end if;
+         end;
+      end if;
+
       SPDM_Proxy.Run;
-   end;
+   end loop;
+
+   --  ISSUE: Componolit/Workarounds#32
+   pragma Warnings (Off, """*"" is set by ""Finalize"" but not used after the call");
+   SPDM_Proxy.Finalize;
+   pragma Warnings (On, """*"" is set by ""Finalize"" but not used after the call");
 exception
    when E : others =>
       Ada.Text_IO.Put_Line (Ada.Exceptions.Exception_Message (E)
--- tests/responder.adb
+++ tests/responder.adb
@@ -1,6 +1,8 @@
 with Server;
 with Channel;
 with RFLX.SPDM_Responder.Session;
+with RFLX.RFLX_Types;
+with RFLX.RFLX_Builtin_Types;
 with Ada.Exceptions;
 with Ada.Text_IO;
 with Ada.Command_Line;
@@ -8,18 +10,54 @@ with GNAT.Traceback.Symbolic;
 
 procedure Responder
 is
-   Listener : Server.Server_Type := Server.Create_Server;
+   use type RFLX.RFLX_Types.Index;
+   use type RFLX.RFLX_Types.Length;
+
+   Listener   : Server.Server_Type := Server.Create_Server;
    Connection : Server.Server_Type;
+   Buffer     : RFLX.RFLX_Types.Bytes (RFLX.RFLX_Types.Index'First .. RFLX.RFLX_Types.Index'First + 4095);
+   Length     : RFLX.RFLX_Builtin_Types.Length;
+
+   package Responder is new RFLX.SPDM_Responder.Session;
+   package TCP_Channel is new Channel (Connection);
 begin
    Server.Listen (Listener, 2324, Connection);
-   declare
-      package TCP_Channel is new Channel (Connection);
-      package Responder is new RFLX.SPDM_Responder.Session (TCP_Channel.Has_Data,
-                                                            TCP_Channel.Receive,
-                                                            TCP_Channel.Send);
-   begin
+   Responder.Initialize;
+   while Responder.Active loop
+      pragma Loop_Invariant (Responder.Initialized);
+      for C in Responder.Channel'Range loop
+         pragma Loop_Invariant (Responder.Initialized);
+         if Responder.Has_Data (C) then
+            declare
+               BS : constant RFLX.RFLX_Types.Length := Responder.Read_Buffer_Size (C);
+            begin
+               if Buffer'Length >= BS then
+                  Responder.Read (C,
+                                  Buffer (Buffer'First .. Buffer'First - 2 + RFLX.RFLX_Types.Index (BS + 1)));
+                  TCP_Channel.Send (Buffer (Buffer'First .. Buffer'First - 2 + RFLX.RFLX_Types.Index (BS + 1)));
+               end if;
+            end;
+         end if;
+
+         if Responder.Needs_Data (C) then
+            declare
+               BS : constant RFLX.RFLX_Types.Length := Responder.Write_Buffer_Size (C);
+            begin
+               TCP_Channel.Receive (Buffer, Length);
+               if Length > 0 and Length <= BS then
+                  Responder.Write
+                     (C,
+                      Buffer (Buffer'First .. Buffer'First +  RFLX.RFLX_Builtin_Types.Index (Length) - 1));
+               end if;
+            end;
+         end if;
+      end loop;
       Responder.Run;
-   end;
+   end loop;
+   --  ISSUE: Componolit/Workarounds#32
+   pragma Warnings (Off, """*"" is set by ""Finalize"" but not used after the call");
+   Responder.Finalize;
+   pragma Warnings (On, """*"" is set by ""Finalize"" but not used after the call");
 exception
    when E : others =>
       Ada.Text_IO.Put_Line (Ada.Exceptions.Exception_Message (E)
