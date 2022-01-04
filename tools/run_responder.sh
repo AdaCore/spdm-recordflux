1410118d5291749f5658bebd6dd505d102e77787
Alexander Senier <senier@componolit.com>
2022-01-04 13:30:24 +0100

    Start spdm requester emu in certificate directory
    Ref. #7

--- tools/run_responder.sh
+++ tools/run_responder.sh
@@ -4,4 +4,4 @@ set -e
 
 build/tests/responder &
 sleep 1 && build/tests/proxy &
-sleep 2 && build/spdm_emu/bin/spdm_requester_emu --trans NONE
+sleep 2 && (cd build/certificates && exec ../spdm_emu/bin/spdm_requester_emu --trans NONE)
