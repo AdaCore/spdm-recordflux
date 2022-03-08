807e6668ec44725d0652e01616cf0de3fcbeac38
Johannes Kliemann <kliemann@adacore.com>
2022-03-08 15:41:26 +0000

    Clean proxy and responder processes after responder test
    ref #70

--- tools/run_responder.sh
+++ tools/run_responder.sh
@@ -3,5 +3,12 @@
 set -e
 
 build/tests/responder &
+RESPONDER_PID=$!
 sleep 1 && build/tests/proxy &
+PROXY_PID=$!
 sleep 2 && (cd build/certificates && exec ../spdm_emu/bin/spdm_requester_emu --trans NONE)
+
+set +e
+kill $RESPONDER_PID > /dev/null 2>&1
+kill $PROXY_PID > /dev/null 2>&1
+exit 0
