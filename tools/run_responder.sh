da0196f863cb7f942db8f1ae40bdf6c0490c49e7
Johannes Kliemann <kliemann@adacore.com>
2022-03-09 09:45:40 +0000

    Add traps for early failure
    ref #75

--- tools/run_responder.sh
+++ tools/run_responder.sh
@@ -4,11 +4,8 @@ set -e
 
 build/tests/responder &
 RESPONDER_PID=$!
+trap "kill $RESPONDER_PID > /dev/null 2>&1" INT SIGINT SIGTERM EXIT
 sleep 1 && build/tests/proxy &
 PROXY_PID=$!
+trap "kill $RESPONDER_PID $PROXY_PID > /dev/null 2>&1" INT SIGINT SIGTERM EXIT
 sleep 2 && (cd build/certificates && exec ../spdm_emu/bin/spdm_requester_emu --trans NONE)
-
-set +e
-kill $RESPONDER_PID > /dev/null 2>&1
-kill $PROXY_PID > /dev/null 2>&1
-exit 0
